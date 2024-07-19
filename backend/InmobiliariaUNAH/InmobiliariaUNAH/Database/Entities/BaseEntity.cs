8fd83a1fc5947db59b602e5d11e05397d572fe63
annerh3 <annerh3@gmail.com>
2024-07-19 16:14:45 -0600

    start of creation of entities

--- backend/InmobiliariaUNAH/InmobiliariaUNAH/Database/Entities/BaseEntity.cs
+++ backend/InmobiliariaUNAH/InmobiliariaUNAH/Database/Entities/BaseEntity.cs
@@ -1,6 +1,40 @@
-﻿namespace InmobiliariaUNAH.Database.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace InmobiliariaUNAH.Database.Entities
 {
     public class BaseEntity
     {
+        [Key] 
+        [Column("id")]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Nombre")]
+        [Required(ErrorMessage = "El {0} de la categoria es requerido.")]
+        [Column("name")]
+        public string Name { get; set; }
+
+        //--------------------
+
+        [StringLength(100)]
+        [Column("created_by")]
+        public string CreatedBy { get; set; }
+        [ForeignKey(nameof(CreatedBy))]
+        // ----------------------
+
+        [Column("created_date")]
+        public DateTime CreatedDate { get; set; }
+
+        // ----------------------
+
+        [StringLength(100)]
+        [Column("edited_by")]
+        public string EditedBy { get; set; }
+        [ForeignKey(nameof(EditedBy))]
+
+        // ----------------------
+
+        [Column("edited_date")]
+        public DateTime EditedDate { get; set; }
     }
 }
---
+++ backend/InmobiliariaUNAH/InmobiliariaUNAH/Database/Entities/ClientTypeEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace InmobiliariaUNAH.Database.Entities
+{
+    public class ClientTypeEntity : BaseEntity
+    {
+        public string Description { get; set; }
+        public decimal Discount { get; set; }
+    }
+}
