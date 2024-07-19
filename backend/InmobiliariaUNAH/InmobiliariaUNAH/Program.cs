9a6bd845ea7b99b3bdd404ad60b2f234fa3df9ce
annerh3 <annerh3@gmail.com>
2024-07-19 12:04:33 -0600

    Base folders are created and nuget packages are installed

--- backend/InmobiliariaUNAH/InmobiliariaUNAH/Program.cs
+++ backend/InmobiliariaUNAH/InmobiliariaUNAH/Program.cs
@@ -1,25 +1,10 @@
+using InmobiliariaUNAH;
+
 var builder = WebApplication.CreateBuilder(args);
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
+var app = builder.Build(); 
+startup.Configure(app, app.Environment);
+// agregar el using
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+app.Run(); 
---
+++ backend/InmobiliariaUNAH/InmobiliariaUNAH/Startup.cs
@@ -0,0 +1,55 @@
+﻿namespace InmobiliariaUNAH
+{
+    public class Startup
+    {
+        private IConfiguration Configuration { get; }
+
+        //Constructor de la clase Startup que recibe la configuración de la aplicación a través de IConfiguration.
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        //  método se utiliza para registrar SERVICIOS en el contenedor de dependencias
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers(); // valida a nivel de controladores
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddControllers().AddNewtonsoftJson(options => // Añadir Controladores con Newtonsoft.Json (del pack: Microsoft.AspNetCore.Mvc.NewtonsoftJson)
+            {
+                options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+            });
+
+            // Add DbContext
+
+
+            // Add custom services
+            // services.AddTransient<ICategoriesService, CategoriesSQLService>();
+
+
+            // Add AutoMapper
+
+        }
+
+        // método se utiliza para configurar el pipeline de solicitud HTTP de la aplicación
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) // Configuracion del Middleware:
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
