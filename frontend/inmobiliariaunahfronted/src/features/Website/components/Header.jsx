464c05d173e3d47fe771ed8411bdc8e5e6c8c07c
Hector Vega <hectormartinez1vg@gmail.com>
2024-08-13 12:07:51 -0600

    files

---
+++ frontend/inmobiliariaunahfronted/src/features/Website/components/Header.jsx
@@ -0,0 +1,19 @@
+export const Header = ({ open }) => {
+    return (
+      <div className="flex gap-x-4 items-center">
+        <img
+          src="./src/assets/logo.png"
+          className={`cursor-pointer duration-500 ${open && "rotate-[360deg]"}`}
+          alt="Logo"
+        />
+        <h1
+          className={`text-white origin-left font-medium text-xl duration-200 ${
+            !open && "scale-0"
+          }`}
+        >
+          Designer
+        </h1>
+      </div>
+    );
+  };
+  
