1af65aeb2ceebd8cffc126638673989199083db6
annerh3 <annerh3@gmail.com>
2024-08-19 02:21:56 -0600

    fixed searchTerm in CreateEventPage, added column to images of the products --A

--- frontend/inmobiliariaunahfronted/src/features/Website/components/events/ProductGrid.jsx
+++ frontend/inmobiliariaunahfronted/src/features/Website/components/events/ProductGrid.jsx
@@ -15,6 +15,7 @@ export const ProductGrid = ({ products, isLoading, onProductSelect }) => {
               <table className="min-w-full divide-y divide-gray-200">
                 <thead className="bg-gray-50">
                   <tr>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Imagen</th>
                     <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nombre</th>
                     <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Categoría</th>
                     <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Costo</th>
@@ -24,7 +25,13 @@ export const ProductGrid = ({ products, isLoading, onProductSelect }) => {
                 <ProductGridItem products={products.data.items} handleDoubleClick={handleDoubleClick} />
               </table>
             ) : (
-              <p className="text-gray-500">No se encontraron productos.</p> 
+              <section className="">
+                <span className="flex text-gray-500 justify-center mb-3">
+                  <img src="https://i.postimg.cc/15KZsqSC/notfound.jpg" alt="404-Not-Found" className="w-80 rounded-md" />
+                </span>
+                <span className="flex text-gray-500 justify-center mb-3">Nel, rey. Dele pa' fuera</span>
+                </section> 
+               
             )}
           </>
         )}
